8c437373834b76c33ebc03fa9fde61392047502f
Damian De Lio <damiandelio@gmail.com>
2018-07-30 15:28:19 -0300

    Fix bugs, agrega descripciones y php
    - Cambia el texto y corrige márgenes.
    
    - Fix bug en Firefox por propiedad absolute.
    
    - Remueve scripts innecesarios.
    
    - Agrega una descripción al footer.
    
    - Agrega favicon
    
    - Optimización para mobile. Mediante php detecta si la petición se
    realiza desde un
      dispositivo mobile y carga un script con menos animaciones.

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Animaciones test</title>
+    <title>Animaciones</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
   </head>
@@ -26,8 +28,12 @@
         </div>
       </div>
       <div class="row">
-        <div class="col m-5 d-flex justify-content-center">
-          <p id="p" style="margin: 6rem 0 3rem 0; max-width: 50rem;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur facilis, asperiores nobis iure unde veritatis libero sequi recusandae. Cum labore earum consectetur quod deserunt aperiam veniam, odio qui iusto cupiditate!</p>
+        <div class="col m-1 m-sm-3 text-center d-flex justify-content-center">
+          <div id="p" style="margin: 8rem 0 5rem 0; max-width: 50rem;">
+            <p>Esta página web está desarrollada con GSAP. Una librería JavaScript que permite realizar animaciones complejas que se renderizan a través de la GPU gracias a la propiedad transform 3D de CSS, obteniendo así un mejor rendimiento.</p>
+            <p>Además utiliza PHP para detectar si se ingresa desde un dispositivo mobile y cargar distintos scripts de animaciones.</p>
+            <p>El layout fue creado con Bootstrap 4.<p>
+          </div>
         </div>
       </div>
       <div class="row justify-content-center">
@@ -39,15 +45,42 @@
         <div id="div-01" class="col-7"></div>
       </div>
     </main>
-    <footer class="container bg-dark" style="height: 25rem">
-
+    <footer class="container bg-dark d-flex align-items-center justify-content-center" style="height: 25rem">
+      <div class="row">
+        <div class="col text-center text-muted">
+          <span>On <a href="https://github.com/damiandelio/GSAP-Animations" class="text-muted footer-link">GitHub</a> • </span>
+          <span>
+            Developed in:<br />
+            Chrome V.68.0.3440.75<br />
+            Firefox V.61.0.1<br />
+          </span>
+        </div>
+      </div>
     </footer>
 
-
+    <!-- bootstrap js -->
+    <!--
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    -->
+
+    <!-- GSAP -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.1/TweenMax.min.js"></script>
-    <script src="./js/js.js"></script>
+
+
+    <!-- Funcion que detecta si es mobile o desktop para cargar las animaciones correctas -->
+    <?php
+      require_once ('functions/isMobile.php');
+
+      if(isMobile()){
+      // Carga las animaciones js-mobile.js
+      echo ('<script src="./js/js-mobile.js"></script>');
+      } else {
+        // Carga las animaciones para desktop
+        echo ('<script src="./js/js.js"></script>');
+      }
+    ?>
+
   </body>
 </html>
--- js/js-mobile.js
+++ js/js-mobile.js
@@ -23,35 +23,8 @@ window.onscroll = () => {
     lastScrollTop = st;
 
     if (window.scrollY < 680) {
-        TweenMax.to("#dl", 0.4, {y: window.scrollY/2, ease: Power3.easeOut});
-        TweenMax.to("#circulo-abc", 0.6, {y: -window.scrollY/6, ease: Power1.easeOut});
-        TweenMax.to("#circulo-abc-2", 0.6, {y: window.scrollY/1.5, ease: Power1.easeOut});
+        TweenMax.to("#dl", 0.4, {y: window.scrollY/2});
     }
-
-    if (window.scrollY > 420) {
-        TweenMax.to(".card", 0.8, {scale: 1, opacity: 1});
-    }
-
-    if (window.innerWidth >= 768) {
-        if (window.scrollY > 750) {
-            TweenMax.to("#div-01", 1, {x: 0, opacity: 1, ease: Power2.easeOut});
-        }
-    } else if (window.innerWidth >= 1200){
-        if (window.scrollY > 1200) {
-            TweenMax.to("#div-01", 1, {x: 0, opacity: 1, ease: Power2.easeOut});
-        }
-    } else {
-        if (window.scrollY > 1500) {
-            TweenMax.to("#div-01", 1, {x: 0, opacity: 1, ease: Power2.easeOut});
-        }
-    }
-
-    // Restablece las transiciones
-    if (window.scrollY < 1) {
-        TweenMax.to(".card", 0, {scale: 2, opacity: 0});
-        TweenMax.to("#div-01", 0, {x: -176, opacity: 0});
-    }
-    //console.log(window.scrollY);
 };
 
 window.onload = () => {
@@ -62,6 +35,5 @@ window.onload = () => {
     }
 };
 
-TweenMax.from("#p", 0.8, {opacity: 0, delay: 1});
-
-//Draggable.create("#dl", {type:"x,y", edgeResistance:0.65, bounds:"#abc", throwProps:true});
+TweenMax.to(".card", 0, {scale: 1, opacity: 1});
+TweenMax.to("#div-01", 0, {x: 0, opacity: 1});
